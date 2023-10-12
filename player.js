e91e08394a27be3ba0a2d63b801862b03b416d39
LauMol <63677546+Lautaromolina@users.noreply.github.com>
2023-10-12 13:14:03 -0300

    Agregando los diferentes estados del personaje. Sprite Animations que correspondan con Jumping running sitting

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js'
 export class Player {
 	constructor(game) {
 		this.game = game
@@ -8,10 +9,17 @@ export class Player {
 		this.vy = 0
 		this.weight = 1
 		this.image = document.getElementById('player')
+		this.frameX = 0
+		this.frameY = 0
 		this.speed = 0
 		this.maxSpeed = 10
+		this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)]
+		this.currentState = this.states[0]
+		this.currentState.enter()
 	}
 	update(input) {
+		//Esto es para cambiar los diferentes estados(spriteAnimationPosition)
+		this.currentState.handleInput(input)
 		//Movimiento Horizontal
 		this.x += this.speed
 		if (input.includes('ArrowRight')) this.speed = this.maxSpeed
@@ -20,15 +28,28 @@ export class Player {
 		if (this.x < 0) this.x = 0
 		if (this.x > this.game.width - this.width) this.x = this.game.width - this.width
 		//Movimiento Vertical
-		if (input.includes('ArrowUp') && this.onGround()) this.vy -= 20
 		this.y += this.vy
 		if (!this.onGround()) this.vy += this.weight
 		else this.vy = 0
 	}
 	draw(context) {
-		context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+		context.drawImage(
+			this.image,
+			this.frameX * this.width,
+			this.frameY * this.height,
+			this.width,
+			this.height,
+			this.x,
+			this.y,
+			this.width,
+			this.height
+		)
 	}
 	onGround() {
 		return this.y >= this.game.height - this.height
+	}
+	setState(state) {
+		this.currentState = this.states[state]
+		this.currentState.enter()
 	}
 }
