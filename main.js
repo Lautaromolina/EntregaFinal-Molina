404894cb5aba71591109da1f0d3d49c7df0e29d0
LauMol <63677546+Lautaromolina@users.noreply.github.com>
2023-10-12 17:39:30 -0300

    Background de 5 layers agregado, corregido el speed y añadido de una nueva clase

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { Player } from './player.js'
 import { InputHandler } from './input.js'
+import { Background } from './background.js'
 
 window.addEventListener('load', function () {
 	const canvas = this.document.getElementById('canvas1')
@@ -12,13 +13,18 @@ window.addEventListener('load', function () {
 			this.width = width
 			this.height = height
 			this.groundMargin = 50
+			this.speed = 0
+			this.maxSpeed = 3
+			this.background = new Background(this)
 			this.player = new Player(this)
 			this.input = new InputHandler()
 		}
 		update(deltaTime) {
+			this.background.update()
 			this.player.update(this.input.keys, deltaTime)
 		}
 		draw(context) {
+			this.background.draw(context)
 			this.player.draw(context)
 		}
 	}
